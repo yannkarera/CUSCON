7ee566f0fc0d85be80945890fed19fababa71280
yannkarera <y_karera@outlook.be>
2025-04-22 10:24:19 +0200

    suppression anciens dossiers

--- CUSCON/dash/theme/listing.php
+++
@@ -1,9 +0,0 @@
-<a href="index.php?type=theme&page=alter">New</a>
-<ul>
-    <?php
-    $stmt = $pdo->query('SELECT name FROM theme');
-    while ($row = $stmt->fetch()) {
-        printf('<li><a href="index.php?type=theme&page=alter&id=%d">%s</a></li>', $row['theme_id'], $row['name']);
-    }
-    ?>
-</ul>
--- CUSCON/public/home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const etoilesElements = document.querySelectorAll(".etoiles");
-    etoilesElements.forEach(function(etoilesElement) {
-      const rating = parseInt(etoilesElement.getAttribute("data-rating"));
-      etoilesElement.style.width = `${rating * 20}%`;
-    });
-  });
-  

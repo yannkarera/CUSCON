c3d79ce8f93629cd3ad3d7a97b9d01c237cda45a
yannkarera <y_karera@outlook.be>
2025-05-20 16:33:48 +0200

    offcanvas navbar mobile

---
+++ public/assets/js/cuscon.js
@@ -0,0 +1,19 @@
+addEventListener("DOMContentLoaded", (event) => { 
+    document.getElementById('menuToggle').addEventListener('click', (e) => {
+        toggleSidebar();
+    })
+})
+
+
+function showSidebar(){
+    document.querySelector('.navbar ul').classList.add('navbarActive')
+}
+
+function hideSidebar(){
+    document.querySelector('.navbar ul').classList.remove('navbarActive')
+}
+
+
+function toggleSidebar(){
+    document.querySelector('.navbar ul').classList.toggle('navbarActive')
+}

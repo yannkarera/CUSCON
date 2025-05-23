67e3d03f942247b910f78de44478201d5a830f5e
yannkarera <y_karera@outlook.be>
2025-05-23 19:39:38 +0200

    ajout fonction panier et favoris

--- app/view/public/catalog_detail.php
+++ app/view/public/catalog_detail.php
@@ -3,6 +3,21 @@ $item = $data['item'];
 $max = $item['stock'];
 
 ?>
+
+  <div class="cart">
+    <h2>Mon Panier</h2>
+    <div id="cartItems"></div>
+    <p><strong>Total:</strong> <span id="totalPrice">0€</span></p>
+  </div>
+
+<div id="favorite-products">
+    <h2>Mes Favoris</h2>
+    <ul id="favoritesList"></ul>
+</div>
+
+
+
+
 <section class="item-container">
     <div class="item-image">
         <img src="/assets/img/manette.jpg" alt="produit">
@@ -11,24 +26,24 @@ $max = $item['stock'];
         <h1><?= ($item['name']) ?></h1>
         <p class="price"><?= ($item['price']) ?> €</p>
         <p class="description"><?= ($item['description']) ?></p>
-        <button class="add-to-cart">Ajouter au panier</button>
-        <button class="add-to-favs">Ajouter au favoris</button>
+       
+        <form action="/favoris/ajouter/<?= ($item['slug']) ?>" method="post">
+            </form>
+            
+            <button  onclick="addToCart('<?= ($item['slug']) ?>', <?= ($item['price']) ?>)" class="add-to-cart">Ajouter au panier</button>
+            <button onclick="addToFavorites('<?= ($item['slug']) ?>')" class="add-to-favs" type="submit">Ajouter au favoris</button>
+
     </div>
     <div class="quantity-selector">
-        <label for="quanitity_<?= $item['stock']?>" >Quantity :</label>
-        <select name="quanitiy" id="quantity_<? $item['stock']?>">
+        <label for="quanitity_<?= $item['stock'] ?>">Quantity :</label>
+        <select name="quanitiy" id="quantity_<? $item['stock'] ?>">
             <?php for ($i = 1; $i <= $max; $i++): ?>
                 <option value="<? $i ?>"><?= $i ?></option>
             <?php endfor; ?>
         </select>
-        
-        
-        
-        
+
+
+
+
     </div>
 </section>
-
-
-
-
-

39fdd9cae4e1cf74ee0d072105495bd91cd64062
yannkarera <y_karera@outlook.be>
2025-05-27 10:07:18 +0200

    fonction de sauvegarde register

--- app/controller/public/GateController.php
+++ app/controller/public/GateController.php
@@ -1,4 +1,5 @@
 <?php
+require_once dirname(__DIR__, 2) . '/model/register.php';
 
 
     function index(){
@@ -11,3 +12,20 @@
 
     render('register.php', ['head_title' => 'Register']);
  }
+
+ function save(){
+    $firstname = $_POST['firstname'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+
+              if (insert($firstname, $name, $email, $pass)) {
+                    render('contact/success.php', ['head_title' => 'MERCI!']);
+                }else{
+                    $data = [];
+                    $data['error'] = 'Une erreur est survenue lors de l\'enregistrement. Veuillez réessayer.';
+                    $data['post_data'] = $_POST;
+                    render('contact/form.php' , $data);
+                }
+                 
+}

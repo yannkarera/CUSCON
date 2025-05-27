93544a75af6a39cd4b5233f24f449a2f77ec8ea2
yannkarera <y_karera@outlook.be>
2025-05-27 10:10:29 +0200

    fonction insert nouveau user dans la db

---
+++ app/model/register.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../config/connection.php';
+
+function insert($firstname, $name, $email, $pass) {
+    $pdo = getConnection();
+    $sql = "INSERT INTO operator (id, role, firstname, name, email, password)VALUES (, 'user', :firstname, :name, :email, :password)";
+    $stmt = $pdo->prepare($sql);
+    return $stmt->execute([
+        ':firstname' => $firstname,
+        ':name' => $name,
+        ':email' => $email,
+        ':password' => $pass
+    ]);
+}

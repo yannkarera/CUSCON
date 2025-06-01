e4beaacf4377031993b04fe55635ab47eb1bb059
yannkarera <y_karera@outlook.be>
2025-06-01 21:15:06 +0200

    ajout page dashboard

---
+++ app/view/admin/dash.php
@@ -0,0 +1,108 @@
+     <h1>Dashboard</h1>
+
+        <div class="date">
+
+          <input type="date">
+
+        </div>
+
+        <div class="insights">
+
+          <div class="sales">
+            <i class="fa-solid fa-arrow-trend-up"></i>
+            <div class="middle">
+              <div class="left">
+                <h3>Total sales</h3>
+              </div>
+              <div class="progress">
+                <svg>
+                  <circle r="30" cy="40" cx="40"></circle>
+                </svg>
+                <div class="number">
+                  80%
+                </div>
+              </div>
+                <small>Last 24 Hours</small>
+              </div>
+
+            </div>
+
+                   <div class="expenses">
+                     <i class="fa-solid fa-receipt"></i>
+                        <div class="middle">
+                          <div class="left">
+                            <h3>Total Expenses</h3>
+                          </div>
+                          <div class="progress">
+                            <svg>
+                              <circle r="30" cy="40" cx="40"></circle>
+                            </svg>
+                            <div class="number">
+                              80%
+                            </div>
+                          </div>
+                            <small>Last 24 Hours</small>
+                          </div>
+
+                        </div>
+
+
+                      <div class="income">
+                      <i class="fa-solid fa-wallet"></i>
+                        <div class="middle">
+                          <div class="left">
+                            <h3>Income</h3>
+                          </div>
+                          <div class="progress">
+                            <svg>
+                              <circle r="30" cy="40" cx="40"></circle>
+                            </svg>
+                            <div class="number">
+                              80%
+                            </div>
+                          </div>
+                            <small>Last 24 Hours</small>
+                          </div>
+
+                        </div>
+              </div>
+
+
+              <div class="recent_order">
+                <h1>Recent Orders</h1>
+                <table>
+                  <thead>
+                  <tr>
+                    <th>Product Name</th>
+                    <th>Product Number</th>
+                    <th>payments</th>
+                    <th>status</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    <td>Mini USB</td>
+                    <td>456</td>
+                    <td>Due</td>
+                    <td class="pending">Pending</td>
+                    <td class="primary">Details</td>
+                  </tbody>
+                   </thead>
+                  <tbody>
+                    <td>Mini USB</td>
+                    <td>456</td>
+                    <td>Due</td>
+                    <td class="pending">Pending</td>
+                    <td class="primary">Details</td>
+                  </tbody>
+                                     </thead>
+                  <tbody>
+                    <td>Mini USB</td>
+                    <td>456</td>
+                    <td>Due</td>
+                    <td class="pending">Pending</td>
+                    <td class="primary">Details</td>
+                  </tbody>
+                </table>
+                
+
+              </div>

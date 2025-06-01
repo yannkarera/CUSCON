2db4b8d57906be3b026694970bd69148b4935122
yannkarera <y_karera@outlook.be>
2025-06-01 21:06:36 +0200

    amélioration fonction d'enregistrement d'operator

--- app/model/register.php
+++ app/model/register.php
@@ -3,7 +3,7 @@ require_once '../config/connection.php';
 
 function insert($firstname, $name, $email, $pass) {
     $pdo = getConnection();
-    $sql = "INSERT INTO operator (id, role, firstname, name, email, password)VALUES (, 'user', :firstname, :name, :email, :password)";
+    $sql = "INSERT INTO operator (firstname, name, email, password)VALUES (:firstname, :name, :email, :password)";
     $stmt = $pdo->prepare($sql);
     return $stmt->execute([
         ':firstname' => $firstname,

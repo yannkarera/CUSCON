c983b665124ef35d60600deb5c77c5b11f6e29ea
yannkarera <y_karera@outlook.be>
2025-06-02 22:49:01 +0200

    ajout fonction enregistrement dans db

--- app/model/register.php
+++ app/model/register.php
@@ -3,7 +3,7 @@ require_once '../config/connection.php';
 
 function insert($firstname, $name, $email, $pass) {
     $pdo = getConnection();
-    $sql = "INSERT INTO operator (id, role, firstname, name, email, password)VALUES (, 'user', :firstname, :name, :email, :password)";
+    $sql = "INSERT INTO operator (firstname, name, email, password, created_at)VALUES (:firstname, :name, :email, :password, NOW())";
     $stmt = $pdo->prepare($sql);
     return $stmt->execute([
         ':firstname' => $firstname,

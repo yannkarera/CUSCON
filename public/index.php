db320a921d91cfe245c071cae9e7af6f49b4b3fa
yannkarera <y_karera@outlook.be>
2025-06-17 12:43:59 +0200

    n

--- public/index.php
+++ public/index.php
@@ -1,32 +1,31 @@
 <?php
 
+session_start();
+
 define('SITE_ROOT', __DIR__ . '/../');
 require_once( __DIR__ . '/../config/config.php');
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-?>
 
-
-<?php
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = trim($uri, '/');
 $uri_parts = explode('/', $uri);
-
 
 $section = 'public';
 if($uri_parts[0] === 'admin'){
     $section = 'admin';
     array_shift($uri_parts);
+
+
 }
 
 $controller = !empty($uri_parts[0]) ? ucfirst($uri_parts[0]) . 'Controller' : 'HomeController';
 $action = $uri_parts[1] ?? 'index';
 $id = null;
 
-
-if(isset( $uri_parts[2])){
-    $id =  $uri_parts[2];
+if(isset($uri_parts[2])){
+    $id = $uri_parts[2];
 }
 
 $controller_path = SITE_ROOT . 'app/controller/'.$section.'/' . $controller . '.php';
@@ -34,12 +33,12 @@ $controller_path = SITE_ROOT . 'app/controller/'.$section.'/' . $controller . '.
 if (file_exists($controller_path)) {
     require_once $controller_path;
 
-        if (function_exists($action)) {
-            call_user_func($action, $id);
-        } else {
-            http_response_code(404);
-            exit("404 - Méthode '$action' non trouvée dans le contrôleur '$controller'.");
-        }
+    if (function_exists($action)) {
+        call_user_func($action, $id);
+    } else {
+        http_response_code(404);
+        exit("404 - Méthode '$action' non trouvée dans le contrôleur '$controller'.");
+    }
 } else {
     http_response_code(404);
     exit("404 - Contrôleur '$controller' non trouvé.");
@@ -56,7 +55,7 @@ function render($partial, $data = [], $section = 'public')
     ob_start();
     include($partial);
     $partial = ob_get_clean();
-    
+
     if(!empty($data['head_title'])){
         $page = str_replace('%%HEAD_TITLE%%', $data['head_title'], $page);
     }
